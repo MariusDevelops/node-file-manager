628746bc1e52d524331bb582b8bf88d6011582a5
MariusDevelops <brazasmarius@gmail.com>
2024-10-13 19:49:30 +0300

    feat: message on exit

--- src/index.js
+++ src/index.js
@@ -3,3 +3,25 @@ const usernameArg = args.find((arg) => arg.startsWith("--username="));
 const username = usernameArg ? usernameArg.split("=")[1] : "User";
 
 console.log(`Welcome to the File Manager, ${username}!`);
+
+const exitHandler = (isCtrlC = false) => {
+    const newline = isCtrlC ? "\n" : "";
+    console.log(
+        `${newline}Thank you for using File Manager, ${username}, goodbye!`
+    );
+    process.exit();
+};
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+    const trimmedData = data.trim();
+
+    if (trimmedData === ".exit") {
+        exitHandler();
+    }
+});
+
+process.on("SIGINT", () => {
+    exitHandler(true);
+});
